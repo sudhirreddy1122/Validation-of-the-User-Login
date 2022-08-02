8e8cb7e54b5e155ce2b1432521dbddd9811f88b1
sudhirreddy1122 <sudhir17072000@gmail.com>
2022-08-03 02:00:08 +0530

    Validation of the User Login

---
+++ ValidiateUserLogin/src/main/java/com/valiateuserlogin/ValidiateServlet.java
@@ -0,0 +1,56 @@
+package com.valiateuserlogin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ValidiateServlet
+ */
+public class ValidiateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ValidiateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		RequestDispatcher rd;
+		if ("admin".equalsIgnoreCase(userName)&& "admin@123".equalsIgnoreCase(password)) {
+			System.out.println("valid user");
+			rd = getServletContext().getRequestDispatcher("/Welcome.html");
+			rd.forward(request,response);
+			
+		}
+		else {
+			System.out.println("Invalid user");
+			rd = getServletContext().getRequestDispatcher("/LoginError.html");
+			rd.forward(request,response);
+		}
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
